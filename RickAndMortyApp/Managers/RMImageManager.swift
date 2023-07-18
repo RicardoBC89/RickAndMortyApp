51c444c87091ae5ae4b69c6b8a873435a4266f39
Ricardo Carvalho <ricardobcarvalho89@gmail.com>
2023-07-18 15:38:45 -0300

    Add image cache

--- RickAndMortyApp/Managers/RMImageManager.swift
+++ RickAndMortyApp/Managers/RMImageManager.swift
@@ -10,15 +10,24 @@ import Foundation
 final class RMImageManager {
     static let shared = RMImageManager()
     
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
     private init() {}
     
     public func donwloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? URLError(.badServerResponse)))
                 return
             }
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
             completion(.success(data))
         }
         task.resume()
